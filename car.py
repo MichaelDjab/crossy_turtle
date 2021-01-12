fe6074782ef4627db9a796a67603390742301369
Michael Djabauri <mich.dja@gmail.com>
2021-01-12 17:30:46 -0500

    Make cars move and add collision functionality

--- car.py
+++ car.py
@@ -6,12 +6,13 @@ class Car(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("square")
+        self.color('#%06X' % random.randint(0, 0xFFFFFF))
         self.shapesize(stretch_wid=1, stretch_len=2)
         self.right(180)
         self.penup()
-        self.goto(random.randint(-320, 320), random.randint(-320, 320))
+        self.goto(random.randint(-310, 1000), random.randint(-280, 300))
 
     def restart(self):
         if self.xcor() < -400:
-            self.goto(400, random.randint(-320, 320))
+            self.goto(random.randint(400, 600), random.randint(-280, 300))
 
